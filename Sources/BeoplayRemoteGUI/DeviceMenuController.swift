c827a45145929d46926bd743d5a7b2802e604b59
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2019-12-03 14:27:57 +0100

    Re-organize for readability

--- Sources/BeoplayRemoteGUI/DeviceMenuController.swift
+++ Sources/BeoplayRemoteGUI/DeviceMenuController.swift
@@ -9,6 +9,7 @@ import Cocoa
 import RemoteCore
 
 class DeviceMenuController : NSObject, NetServiceDelegate {
+    private let resolveTimeout = 5.0
     private let statusMenu: NSMenu
     private let remoteControl: RemoteControl
     private let deviceSeparatorMenuItem: NSMenuItem
@@ -51,52 +52,61 @@ class DeviceMenuController : NSObject, NetServiceDelegate {
     }
 
     public func handleDeviceUpdates(_ updates: [DeviceCommand]) {
-        DispatchQueue.main.async {
+        func removeDevice(_ device: NetService) {
+            NSLog("removeDevice: \(device.name)")
             
+            if let item = self.getMenuItem(device) {
+                self.statusMenu.removeItem(item)
+                self.didUpdateDevices()
+            }
+        }
+
+        func addDevice(_ device: NetService) {
+
+            func getLocationForNew(_ newItem: NSMenuItem) -> Int {
+                for item in getDeviceMenuItems() {
+                    if newItem.title < item.title {
+                        return statusMenu.index(of: item)
+                    }
+                }
+                return self.statusMenu.index(of: self.deviceSeparatorMenuItem)
+            }
+
+            NSLog("addDevice: \(device.name)")
+
+            let item = NSMenuItem(title: device.name, action: #selector(deviceClicked(_:)), keyEquivalent: "")
+            item.representedObject = device
+            item.target = self
+            item.isEnabled = false
+
+            self.statusMenu.insertItem(item, at: getLocationForNew(item))
+
+            // The menu item is enabled by the delegate when the service address has been resolved
+            device.delegate = self
+            device.resolve(withTimeout: self.resolveTimeout)
+        }
+
+        DispatchQueue.main.async {
             for update in updates {
                 let menuHasDevice = self.statusMenu.indexOfItem(withRepresentedObject: update.device) > -1
-            
+
                 switch update.type {
                 case DeviceAction.Add:
                     if menuHasDevice == false {
-                        self.addDevice(update.device)
+                        addDevice(update.device)
                     }
                 case DeviceAction.Remove:
-                    self.removeDevice(update.device)
+                    removeDevice(update.device)
                 }
             }
         }
     }
 
-    func removeDevice(_ device: NetService) {
-        NSLog("removeDevice: \(device.name)")
-        
-        if let item = self.getMenuItem(device) {
-            self.statusMenu.removeItem(item)
-            self.didUpdateDevices()
-        }
-    }
-
-    func addDevice(_ device: NetService) {
-        NSLog("addDevice: \(device.name)")
-    
-        let item = NSMenuItem(title: device.name, action: #selector(deviceClicked(_:)), keyEquivalent: "")
-        item.representedObject = device
-        item.target = self
-        item.isEnabled = false
-        
-        self.statusMenu.insertItem(item, at: self.getLocationForNew(item))
-
-        // The menu item is enabled by the delegate when the service address has been resolved
-        device.delegate = self
-        device.resolve(withTimeout: TimeInterval(5.0))
-    }
-
-    func netServiceDidResolveAddress(_ service: NetService) {
+    func netServiceDidResolveAddress(_ device: NetService) {
         DispatchQueue.main.async {
-            NSLog("netServiceDidResolveAddress: \(service.hostName!)")
+            NSLog("netServiceDidResolveAddress: \(device.hostName!)")
 
-            let menuItem = self.getMenuItem(service)
+            let menuItem = self.getMenuItem(device)
 
             menuItem?.isEnabled = true
             self.didUpdateDevices()
@@ -114,10 +124,13 @@ class DeviceMenuController : NSObject, NetServiceDelegate {
             self.sourcesMenuController.noDevicesAvailable()
         default:
             guard let defaultDevice = UserDefaults.standard.string(forKey: "devices.default") else {
+                // Do nothing unless a default device is configured
                 return
             }
 
-            if self.getSelected(menuItems).count == 0 {
+            let selectedItems = menuItems.filter { $0.state == NSControl.StateValue.on }
+
+            if selectedItems.count == 0 {
                 if let item = menuItems.filter({ $0.title == defaultDevice }).first {
                     NSLog("connecting to default device")
                     self.connectDevice(item)
@@ -126,42 +139,26 @@ class DeviceMenuController : NSObject, NetServiceDelegate {
         }
     }
 
-    private func getLocationForNew(_ newItem: NSMenuItem) -> Int {
-        for item in getDeviceMenuItems() {
-            if newItem.title < item.title {
-                return statusMenu.index(of: item)
-            }
-        }
-        return self.statusMenu.index(of: self.deviceSeparatorMenuItem)
-    }
-
-    private func getDeviceMenuItems() -> [NSMenuItem] {
+    func getDeviceMenuItems() -> [NSMenuItem] {
         self.statusMenu.items.filter { $0.representedObject is NetService }
     }
-
-    private func getSelected(_ items: [NSMenuItem]) -> [NSMenuItem] {
-        items.filter { $0.state == NSControl.StateValue.on }
-    }
         
-    private func getMenuItem(_ device: NetService) -> NSMenuItem? {
+    func getMenuItem(_ device: NetService) -> NSMenuItem? {
         let location = self.statusMenu.indexOfItem(withRepresentedObject: device)
         return self.statusMenu.item(at: location)
     }
 
-    private func connectDevice(_ sender: NSMenuItem) {
+    func connectDevice(_ sender: NSMenuItem) {
         guard let device = sender.representedObject as? NetService else {
+            NSLog("connectDevice panic")
             return
         }
 
         NSLog("connectDevice \"\(device.name)\", \(device.hostName!):\(device.port)")
 
-        self.remoteControl.stopVolumeNotifications()
+        self.remoteControl.stopNotifications()
         self.remoteControl.setEndpoint(host: device.hostName!, port: device.port)
-
-        // read the current volume level and receive updates on future volume levels
-        self.remoteControl.receiveVolumeNotifications(volumeUpdate: self.volumeLevelViewController.receiveVolumeUpdate,
-                                                      connectionUpdate: self.connectionUpdate(state:message:))
-
+        self.remoteControl.startNotifications()
         self.selectDeviceMenuItem(sender)
 
         if UserDefaults.standard.bool(forKey: "sources.enabled") {
